09e8a4b504e7be4464eff94604c1c3dc020f2b07
Geovany Gameros <geovanygameros@hotmail.com>
2016-11-12 15:36:17 -0700

    Barra de busqueda optimizada, busca coincidencias en title y en text

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   http_basic_authenticate_with name: "admin", password: "admin", except: [:index, :show]
 
   def index
-    @articles = Article.where("title LIKE ?" , "%#{params[:search]}%")
+    @articles = Article.where("title LIKE ? OR text LIKE?" , "%#{params[:search]}%", "%#{params[:search]}%")
   end
 
 
