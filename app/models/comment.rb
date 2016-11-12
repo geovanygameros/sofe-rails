e8ef37e676043126dad7ad772abe640f1990fee1
Alejandro Escobedo Garcia <aeg0204@gmail.com>
2016-11-12 14:13:56 -0700

    Story #3

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,11 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
+  before_create :jadenFilter
+
+  def jadenFilter
+    if self.body == self.body.upcase
+      self.body = self.body.downcase.split.map(&:capitalize).join(' ')
+    end
+  end
+
 end
